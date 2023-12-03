a16760f2904adfe5dafccf34a7cd1faf735a637a
Tay Nguyen <pttnguyen528@gmail.com>
2023-12-03 11:04:00 +0700

    update code

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,38 +12,38 @@ window.onscroll = function () {
 
 // SEARCH BTN
 
-document.querySelector('.search-open').onclick = function () {
-    document.querySelector('body').classList.add('search-active');
-    document.querySelector('.input-search').focus();
+document.querySelector(".search-open").onclick = function () {
+    document.querySelector("body").classList.add("search-active");
+    document.querySelector(".input-search").focus();
 };
 
-document.querySelector('.search-close').onclick = function () {
-    document.querySelector('body').classList.remove('search-active');
+document.querySelector(".search-close").onclick = function () {
+    document.querySelector("body").classList.remove("search-active");
 };
 
 //  CHANGE THEME
 
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-const currentTheme = localStorage.getItem('theme');
+const toggleSwitch = document.querySelector(
+    '.theme-switch input[type="checkbox"]'
+);
+const currentTheme = localStorage.getItem("theme");
 
 if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
+    document.documentElement.setAttribute("data-theme", currentTheme);
 
-    if (currentTheme === 'dark') {
+    if (currentTheme === "dark") {
         toggleSwitch.checked = true;
     }
 }
 
 function switchTheme(e) {
     if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
-    }
-    else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light');
+        document.documentElement.setAttribute("data-theme", "dark");
+        localStorage.setItem("theme", "dark");
+    } else {
+        document.documentElement.setAttribute("data-theme", "light");
+        localStorage.setItem("theme", "light");
     }
 }
 
-toggleSwitch.addEventListener('change', switchTheme, false);
-
+toggleSwitch.addEventListener("change", switchTheme, false);
